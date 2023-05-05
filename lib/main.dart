5f2151ee2930de07f9afccd054b5847603f293f2
noushinizar <noushida.maitexa@gmail.com>
2023-05-05 15:56:03 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Center(
+      child: Text(
+    'Maitexa',
+    textDirection: TextDirection.ltr,
+    style: TextStyle(
+        color: Colors.blue,
+        fontSize: 35,
+        fontWeight: FontWeight.bold,
+        fontStyle: FontStyle.italic),
+  )));
+}
